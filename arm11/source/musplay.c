7f3188726a2863b4f5816d56d14f353398747c2b
CyberWavy <78249475+CyberWavy@users.noreply.github.com>
2021-01-30 17:37:50 +1100

    Change MUS_PLAYBACK_RATE from 11025 to 44100

--- arm11/source/musplay.c
+++ arm11/source/musplay.c
@@ -21,7 +21,7 @@
 #include "w_wad.h"
 
 #define MUS_MIX_CHANNEL 23
-#define MUS_PLAYBACK_RATE 11025
+#define MUS_PLAYBACK_RATE 44100 // There. It sounds good
 
 void mixer_update(short *pAudioData, int count);
 int mixer_pos();
